afe89c20c5eaf6f8ada1ac529edcf658f7a18cd0
Pierre Bourdon <delroth@gmail.com>
2019-09-09 06:12:16 +0200

    eval-machines: remove .drv file which pulls in unneeded deps

--- data/eval-machines.nix
+++ data/eval-machines.nix
@@ -88,7 +88,6 @@ rec {
         mkdir -p $out
         ${toString (mapAttrsToList (nodeName: nodeDef: ''
           ln -s ${nodeDef.config.system.build.toplevel} $out/${nodeName}
-          ln -s ${nodeDef.config.system.build.toplevel.drvPath} $out/${nodeName}.drv
         '') nodes')}
       ''
       else ''
